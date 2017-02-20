6f05acaf969cad516a094980511a77b77c88e4c6
micho <gatin13@yahoo.com>
2017-02-20 15:48:49 +0900

    Fixed issues with view file names

--- app/Module.scala
+++ app/Module.scala
@@ -18,8 +18,7 @@ class Module extends AbstractModule {
   override def configure() = {
     // Use the system clock as the default implementation of Clock
     bind(classOf[Clock]).toInstance(Clock.systemDefaultZone)
-    // Ask Guice to create an instance of ApplicationTimer when the
-    // application starts.
+    // Ask Guice to create an instance of ApplicationTimer when the application starts.
     bind(classOf[ApplicationTimer]).asEagerSingleton()
     // Set AtomicCounter as the implementation for Counter.
     bind(classOf[Counter]).to(classOf[AtomicCounter])
