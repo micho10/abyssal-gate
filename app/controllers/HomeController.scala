169f5c8797f20e654fa9d1f1a92771f786009967
micho10 <gatin13@yahoo.com>
2017-09-21 15:49:57 +0900

    Applied styles

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -30,7 +30,7 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
     * Create an Action to render an HTML page.
     *
     * The configuration in the `routes` file means that this method will be called when the application receives a `GET` request
-    * with a path of `/`.
+    * with a path of `/index2`.
     */
   def index2() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index2())
