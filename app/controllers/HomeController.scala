ed045dbe9256a9361da1ebdd2950b63d41887794
micho10 <gatin13@yahoo.com>
2017-09-21 15:11:20 +0900

    Initial adaptation of the home page to Twirl templates

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -24,4 +24,16 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index("Your new application is ready!"))
   }
+
+
+  /**
+    * Create an Action to render an HTML page.
+    *
+    * The configuration in the `routes` file means that this method will be called when the application receives a `GET` request
+    * with a path of `/`.
+    */
+  def index2() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.index2())
+  }
+
 }
