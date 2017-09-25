326693708d854d6e64a24f0534b561dc57478f11
micho10 <gatin13@yahoo.com>
2017-09-25 18:25:02 +0900

    Stylesheet comments and improvements

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,9 +8,9 @@
 
     skel.breakpoints({
         xlarge: '(max-width: 1680px)',
-        large: '(max-width: 1280px)',
+        large:  '(max-width: 1280px)',
         medium: '(max-width: 980px)',
-        small: '(max-width: 736px)',
+        small:  '(max-width: 736px)',
         xsmall: '(max-width: 480px)'
     });
 
